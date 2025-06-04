25df4ed0d83bce4bdef480eed094af3165304583
volkantemelli <volkan.temelli@gmail.com>
2025-06-04 15:51:13 +0200

    Refactor input handling to improve vocabulary processing and update HTML structure

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 "use strict";
-
+// Store DOM elements in variables
 const textArea = document.querySelector("#vocabulary-input");
 const sendButton = document.querySelector(".send-button");
 
-sendButton.addEventListener("click", () => {
-  const textAreaValue = textArea.value;
-  console.log(textAreaValue);
-});
+function randomOrder() {
+  // Store turkish words input into a constant named turkishWords
+  const turkishWords = textArea.value;
+  // Trim the end of input just in case and split it into different strings by new row mark
+  const turkishWordsArr = turkishWords.trim().split("\n");
+  console.log(turkishWordsArr);
+}
+
+sendButton.addEventListener("click", randomOrder);
