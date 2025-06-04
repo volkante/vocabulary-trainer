6cae0cbae590c06394ee6b264307c0c856183c8a
volkantemelli <volkan.temelli@gmail.com>
2025-06-04 16:59:34 +0200

    Refactor randomOrder function to initialize randomWordsObj and log random number

--- script.js
+++ script.js
@@ -10,8 +10,15 @@ function randomOrder() {
   // Trim the blank space at the beginning and end of input just in case and split it into different strings by new row mark
   const turkishWordsArr = turkishWords.trim().split("\n");
 
+  //
+  const randomWordsObj = {};
+  //
+  const randomNum = Math.floor(Math.random() * 6 + 1);
+  console.log(randomNum);
+
   // Iterate over the turkish words to sort them randomly
   for (let i = 0; i < turkishWordsArr.length; i++) {
+    randomWordsObj[i] = -1;
     // Create a list element for each word
     const turkishWordElement = document.createElement("li");
     // Take the list element's text content from turkish words arr
@@ -19,6 +26,7 @@ function randomOrder() {
     // Add list element to its parent to display it
     outputList.appendChild(turkishWordElement);
   }
+  console.log(randomWordsObj);
 }
 
 sendButton.addEventListener("click", randomOrder);
