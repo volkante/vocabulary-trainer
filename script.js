e948a9372da27e62f428f3f7d1c7e2d50524f22c
volkantemelli <volkan.temelli@gmail.com>
2025-06-06 13:41:26 +0200

    Refactor randomOrder function to move createRandomNum definition inside for better encapsulation

--- script.js
+++ script.js
@@ -9,17 +9,14 @@ const turkishWords = textArea.value;
 // Trim the blank space at the beginning and end of input just in case and split it into different strings by new row mark
 const turkishWordsArr = turkishWords.trim().split("\n");
 
-function createRandomNum() {
-  const randomNum = Math.floor(Math.random() * turkishWordsArr.length);
-  return randomNum;
-}
-
 function randomOrder() {
   // Create a random numbers array to store random numbers
   const randomNumArr = [];
+  // Add random numbers to the random numbers array by calling create random number function
   for (let i = 0; i < turkishWordsArr.length; i++) {
     randomNumArr.push(createRandomNum());
   }
+
   console.log(randomNumArr);
   // Iterate over the turkish words to sort them randomly
   for (let i = 0; i < turkishWordsArr.length; i++) {
@@ -31,6 +28,17 @@ function randomOrder() {
     turkishWordElement.textContent = turkishWordsArr[randomNumArr[i]];
     // Add list element to its parent to display it
     outputList.appendChild(turkishWordElement);
+  }
+
+  // Declare a function to create a random num
+  function createRandomNum() {
+    // Create a random number
+    let randomNum = Math.floor(Math.random() * turkishWordsArr.length);
+    // If the random number is already found in random number array, recreate a random number
+    while (randomNumArr.indexOf(randomNum) !== -1) {
+      randomNum = Math.floor(Math.random() * turkishWordsArr.length);
+    }
+    return randomNum;
   }
 }
 
