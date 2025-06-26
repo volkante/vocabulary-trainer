438f7982671d0ee1f69ed87ef20d98ff9db93825
volkantemelli <volkan.temelli@gmail.com>
2025-06-26 19:57:01 +0200

    Refactor variable naming in onLoad function for clarity and consistency

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -1,10 +1,10 @@
 import { csvOutput } from "./domElements.js";
 
-let json;
+let jsonResult;
 export function onLoad(e) {
   console.log(e);
   const csvText = e.target.result;
-  json = CSVJSON.csv2json(csvText, { parseNumbers: true });
-  console.log("Dönüştürülmüş JSON:", json);
-  csvOutput.textContent = json;
+  jsonResult = CSVJSON.csv2json(csvText, { parseNumbers: true });
+  console.log("Dönüştürülmüş JSON:", jsonResult);
+  csvOutput.textContent = jsonResult[0]["Satz/Wort"];
 }
