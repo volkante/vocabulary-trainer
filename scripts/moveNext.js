a284f8cbf481bc83504ac9adcf9e4d932e5d30c3
volkantemelli <volkan.temelli@gmail.com>
2025-07-01 19:58:07 +0200

    Remove unused code and delete revealNext.js file to streamline functionality

--- scripts/moveNext.js
+++ scripts/moveNext.js
@@ -3,13 +3,6 @@
 let indexOfDisplayedWord = 0;
 
 export function moveNext(arr, element) {
-  const wordsArr = [];
-
-  console.log(Object.values(arr[0]));
-  /* for(let i = 0; i < arr.length; i++){
-    arr[0].entries();
-  } */
-
   element.replaceChildren();
   const wordElement = document.createElement("li");
   wordElement.textContent = arr[indexOfDisplayedWord]["Bedeutung"];
