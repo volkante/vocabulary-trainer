464dbdde7b099f41cd6d0113d78c1d0924e1337d
volkantemelli <volkan.temelli@gmail.com>
2025-07-01 19:42:05 +0200

    Update button label from 'Reveal next' to 'Next' for consistency

--- scripts/moveNext.js
+++ scripts/moveNext.js
@@ -3,6 +3,13 @@
 let indexOfDisplayedWord = 0;
 
 export function moveNext(arr, element) {
+  const wordsArr = [];
+
+  console.log(Object.values(arr[0]));
+  /* for(let i = 0; i < arr.length; i++){
+    arr[0].entries();
+  } */
+
   element.replaceChildren();
   const wordElement = document.createElement("li");
   wordElement.textContent = arr[indexOfDisplayedWord]["Bedeutung"];
