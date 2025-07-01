c833ab8aae49e5f0f1b3ee8eeec2eab465276a25
volkantemelli <volkan.temelli@gmail.com>
2025-07-01 16:43:46 +0200

    Update moveNext function to correctly display word meaning from array

--- scripts/moveNext.js
+++ scripts/moveNext.js
@@ -5,7 +5,7 @@ let indexOfDisplayedWord = 0;
 export function moveNext(arr, element) {
   element.replaceChildren();
   const wordElement = document.createElement("li");
-  wordElement.textContent = arr[indexOfDisplayedWord];
+  wordElement.textContent = arr[indexOfDisplayedWord]["Bedeutung"];
   element.appendChild(wordElement);
   indexOfDisplayedWord++;
 }
