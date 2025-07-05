918d5a7deec04b8d0a8b4f0c2710c1807ba84073
volkantemelli <volkan.temelli@gmail.com>
2025-07-05 22:28:51 +0300

    Refactor moveNext function to create links for URL content; improve readability and structure

--- scripts/moveNext.js
+++ scripts/moveNext.js
@@ -5,7 +5,15 @@ let indexOfDisplayedWord = 0;
 export function moveNext(arr, element) {
   element.replaceChildren();
   const wordElement = document.createElement("li");
-  wordElement.textContent = arr[indexOfDisplayedWord];
+  const content = arr[indexOfDisplayedWord];
+  if (content.startsWith("https")) {
+    const link = document.createElement("a");
+    link.href = content;
+    link.textContent = content;
+    wordElement.appendChild(link);
+  } else {
+    wordElement.textContent = content;
+  }
   element.appendChild(wordElement);
   indexOfDisplayedWord++;
 }
