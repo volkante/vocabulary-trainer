db30779f91c1267b9743d24bc1582ba879a21380
volkantemelli <volkan.temelli@gmail.com>
2025-07-17 21:18:14 +0300

    Fix typo in "use strict" and refactor link creation in moveNext function for improved clarity

--- scripts/moveNext.js
+++ scripts/moveNext.js
@@ -1,4 +1,4 @@
-"use script";
+"use strict";
 
 let indexOfDisplayedWord = 0;
 
@@ -6,14 +6,20 @@ export function moveNext(arr, element) {
   element.replaceChildren();
   const wordElement = document.createElement("li");
   const content = arr[indexOfDisplayedWord];
+  // If revealed info is a link, make it anchor element
   if (content.startsWith("https")) {
-    const link = document.createElement("a");
-    link.href = content;
-    link.textContent = content;
-    wordElement.appendChild(link);
+    createLink(content, wordElement);
   } else {
     wordElement.textContent = content;
   }
   element.appendChild(wordElement);
   indexOfDisplayedWord++;
+}
+
+function createLink(content, wordElement) {
+  const link = document.createElement("a");
+  link.href = content;
+  link.textContent = content;
+  link.target = "_blank";
+  wordElement.appendChild(link);
 }
