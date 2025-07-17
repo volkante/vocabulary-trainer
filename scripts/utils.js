1f0957928b2dc647e85a57fe824fd5eef9d5b38a
volkantemelli <volkan.temelli@gmail.com>
2025-07-17 18:50:23 +0300

    Refactor convertObjectsToArr to swap Turkish meaning and key; improve clarity of entries handling

--- scripts/utils.js
+++ scripts/utils.js
@@ -10,9 +10,13 @@ export function getArrayFromCorrectedInput(inputVal) {
 export function convertObjectsToArr(arr) {
   const wordInfosArr = [];
   // Iterate over objects of array
+  console.log(Object.entries(arr[0]));
   for (let i = 0; i < arr.length; i++) {
     // Create entries array which includes another array of the key and value of each word info
     const entries = Object.entries(arr[i]);
+    const turkishMeaning = entries[1];
+    entries[1] = entries[0];
+    entries[0] = turkishMeaning;
     // Iterate over entries array
     for (let j = 0; j < 4; j++) {
       let value = entries[j][1];
@@ -28,3 +32,9 @@ function wordInfoCreate(key, val) {
   const wordInfo = val === "" ? `keine ${key}` : val;
   return wordInfo;
 }
+
+/* function swapGermanWordTurkishMeaning (arr) {
+    const germanWord = arr[0];
+    arr[1] = arr[0];
+    arr[0] = germanWord;
+} */
