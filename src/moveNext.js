d89925e6487c30f65dfba31afa068e4a7bbc0229
volkantemelli <volkan.temelli@gmail.com>
2025-09-12 20:40:43 +0200

    fix output title change function

--- src/moveNext.js
+++ src/moveNext.js
@@ -2,6 +2,9 @@ import { outputTitle } from "./domElements.js";
 import { getIndexOfWordObj, getlastCsvJsonResult } from "./state.js";
 
 let wordInfoIndex = -1;
+let wordIndexObj = 0;
+let objectPropertyLength;
+
 export function moveNext(wordObjArr, element) {
   // Prevent index of displayed word from exceeding the wordinfos array: If index is at the last, show alert and return from the function.
   if (getIndexOfWordObj() >= getlastCsvJsonResult().length - 1) {
@@ -9,15 +12,23 @@ export function moveNext(wordObjArr, element) {
     return;
   }
 
-  let objectPropertyLength = Object.entries(wordObjArr[0]).length;
+  objectPropertyLength = Object.entries(wordObjArr[0]).length;
+  console.log("objectpropertylength", objectPropertyLength);
 
   wordInfoIndex++;
 
-  const wordInfos = Object.entries(wordObjArr[0]);
+  if (wordInfoIndex === objectPropertyLength) {
+    console.log(wordInfoIndex === objectPropertyLength);
+    wordIndexObj++;
+    wordInfoIndex = 0;
+  }
 
-  outputTitle.textContent = wordInfos[wordInfoIndex][0];
+  const wordInfos = Object.entries(wordObjArr[wordIndexObj]);
 
   console.log(wordInfos);
+  console.log("wordinfoindex", wordInfoIndex);
+  console.log("key", wordInfos[wordInfoIndex]);
+  outputTitle.textContent = wordInfos[wordInfoIndex][0];
 
   /*   setIndexOfDisplayedWordInfo(getIndexOfDisplayedWordInfo() + 1);
   // Increase word index and display it
